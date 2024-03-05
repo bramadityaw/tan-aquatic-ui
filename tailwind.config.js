8f7ffce2fa99a40afb40d98e088c5c6c4aa2817b
bramaditya <bramadityaw@duck.com>
2024-03-06 03:40:14 +0700

    navbar implemented

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}", 
+	    "toko/*.{html,js}",
+	    "login/*.{html,js}",
+	    "blog/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
